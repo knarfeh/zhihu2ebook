daeae206101b1d2a9ade5c08cbb05b1ca54782f5
knarf <hejun1874@163.com>
2016-03-08 20:00:48 +0800

    add jianshu to type.py

--- src/tools/type.py
+++ src/tools/type.py
@@ -30,10 +30,24 @@ class Type(object):
 
     SinaBlog_type_list = ['SinaBlog', 'SinaBlogAuthor']
 
-    # jianshu
+    # jianshu    # TODO, 目前只有latest_articles一种, 还可以写collections, notebook等等
+    jianshu_article = 'jianshu_article'     # 类型是单篇的文章   TODO
+    jianshu = 'jianshu'                     # 类型是简书文章的集锦
+
+    jianshu_info = 'jianshu_info'
+
+    jianshu_article_type_list = ['jianshu']
+
+    info_table = {
+        'jianshu_info': jianshu_info
+    }
+    jianshu_type_list = [
+        'jianshu',
+    ]
 
     # 文章必须放在专栏之前（否则检测类别的时候就一律检测为专栏了） TODO how's that?
-    type_list = question_type_list + article_type_list + SinaBlog_type_list
+    type_list = question_type_list + article_type_list + SinaBlog_type_list + jianshu_type_list
+
     info_table = {
         column: column_info,
         author: author_info,
