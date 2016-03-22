d65fe23506b744a6d8cfcc2227fadf0f31b7caad
knarf <hejun1874@163.com>
2016-03-22 17:09:15 +0800

    A: helpform

--- EEBook.py
+++ EEBook.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# 放置于首位
 import sys  # 修改默认编码
 import os   # 添加系统路径
 reload(sys)
@@ -8,6 +7,7 @@ base_path = unicode(os.path.abspath('.').decode(sys.stdout.encoding))
 # print base_path
 sys.path.append(base_path + '/src/lib')
 
+# 早期版本可以直接sys.setdefaultencoding('utf-8')，新版本需要先reload一下
 sys.setdefaultencoding('utf-8')
 sys.setrecursionlimit(100000)  # 为BS解析知乎上的长答案增加递归深度限制
 
