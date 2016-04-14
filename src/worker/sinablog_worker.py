6029853f3914a68c1a0e3cc1701e237656205ca0
knarf <hejun1874@163.com>
2016-04-14 21:05:26 +0800

    Modify sinablog worker

--- src/worker/sinablog_worker.py
+++ src/worker/sinablog_worker.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json
-
 from bs4 import BeautifulSoup
 
 from src.tools.http import Http
 from src.tools.match import Match
-from src.tools.config import Config
 from src.worker.page_worker import PageWorker
 
-from src.lib.sinablog_parser.sinablogparser import SinaBlogParser
+from src.lib.sinablog_parser.sinablog_parser import SinaBlogParser
 from src.lib.parser_tools import ParserTools
 
 
@@ -21,15 +18,16 @@ class sinablogAuthorWorker(PageWorker):
     @staticmethod
     def parse_max_page(content):
         u"""
+        TODO: useless?? delete??
         :param content: 博客目录的页面内容
         :return: max page
         """
         max_page = 1
         try:
-             floor = content.index('">下一页</a>')
-             floor = content.rfind('</a>', 0, floor)
-             cell = content.rfind('>', 0, floor)
-             max_page = int(content[cell + 1:floor])
+            floor = content.index('">下一页</a>')
+            floor = content.rfind('</a>', 0, floor)
+            cell = content.rfind('>', 0, floor)
+            max_page = int(content[cell + 1:floor])
         finally:
             return max_page
 
@@ -45,7 +43,7 @@ class sinablogAuthorWorker(PageWorker):
     def parse_article_num(content):
         u"""
 
-        :param target_url: 博客目录的content
+        :param content: 博客目录的content
         :return: 博文总数量
         """
         soup = BeautifulSoup(content, "lxml")
@@ -57,8 +55,8 @@ class sinablogAuthorWorker(PageWorker):
     @staticmethod
     def parse_get_article_list(article_list_content):
         u"""
-        获得每一篇博客的链接组成的列表
-        :param article_list_content: 博文目录页面的内容
+        get the list of blog href
+        :param article_list_content: list page
         :return:
         """
         soup = BeautifulSoup(article_list_content, "lxml")
@@ -71,12 +69,26 @@ class sinablogAuthorWorker(PageWorker):
 
         return article_href_list
 
+    def get_sinablog_question_list(self, author_id):
+        u"""
+        get sinablog_info, article_num
+        :param author_id:
+        :return:
+        """
+        href_article_list = 'http://blog.sina.com.cn/s/articlelist_{}_0_1.html'.format(author_id)
+        href_profile = 'http://blog.sina.com.cn/s/profile_{}.html'.format(author_id)
+        content_profile = Http.get_content(href_profile)
+        parser = SinaBlogParser(content_profile)
+        self.question_list += parser.get_sinablog_info_list()
+        content_article_list = Http.get_content(href_article_list)
+        article_num = int(self.parse_article_num(content_article_list))
+        return article_num
+
     def create_work_set(self, target_url):
         u"""
         根据博客首页的url, 首先通过re获得博客id, 然后根据博客"关于我"的页面的内容获得写入sinablog_info
         的数据(这部分理应不在这个函数中, 可以改进), 最后通过博客目录页面的内容, 获得每篇博文的地址,
         放入work_set中
-
         :param target_url: 博客首页的url
         :return:
         """
@@ -85,28 +97,14 @@ class sinablogAuthorWorker(PageWorker):
         result = Match.sinablog_author(target_url)
         sinablog_author_id = int(result.group('sinablog_people_id'))
 
-        href_article_list = 'http://blog.sina.com.cn/s/articlelist_{}_0_1.html'.format(sinablog_author_id)
-        href_profile = 'http://blog.sina.com.cn/s/profile_{}.html'.format(sinablog_author_id)
-
-        # ############下面这部分应该是SinaBlogAuthorWorker的内容, 写到sinablog_info, 暂时写在这, 以后再优化
-        content_profile = Http.get_content(href_profile)
-
-        parser = SinaBlogParser(content_profile)
-        self.question_list += parser.get_sinablog_info_list()
-        # Debug.logger.debug(u"create_work_set中的question_list是什么??" + str(self.question_list))
-        # #############上面这部分应该是SinaBlogAuthorWorker的内容, 写到sinablog_info, 暂时写在这, 以后再优化
-
-        # content_index = Http.get_content(href_index)
-        content_article_list = Http.get_content(href_article_list)
-
-        article_num = int(self.parse_article_num(content_article_list))
+        article_num = self.get_sinablog_question_list(sinablog_author_id)
         if article_num % 50 != 0:
-            page_num = article_num/50 + 1      # 博客目录页面, 1页放50个博客链接
+            page_num = article_num/50 + 1      # 50 href on 1 page
         else:
             page_num = article_num / 50
 
-        self.question_list[0]['article_num'] = article_num  # 这样的话, 每行只能放一个新浪博客地址!!!
-        # 上面这行, 暂时只能这样写, 因为"关于我"的页面, 没有文章的数量
+        self.question_list[0]['article_num'] = article_num
+        # 上面这行, 暂时只能这样写, 因为"关于我"的页面没有文章的数量
 
         self.task_complete_set.add(target_url)
 
@@ -116,5 +114,4 @@ class sinablogAuthorWorker(PageWorker):
             article_list = self.parse_get_article_list(content_article_list)
             for item in article_list:
                 self.work_set.add(item)
-            # self.work_set.add(article_list[0])
         return
